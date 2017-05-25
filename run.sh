1ee417e2775171508dd8df163f7afb776e742471
Schnitzel <michael@amazee.com>
2017-05-25 16:49:15 -0500

    only run 3 times, better documentation

--- run.sh
+++ run.sh
@@ -15,15 +15,15 @@ function runTest {
     echo "### $1: starting container"
     printtime docker-compose up -d --force
 
-    echo "### $1: drush site install 5x"
-    for i in `seq 1 5`;
+    echo "### $1: drush site install 3x"
+    for i in `seq 1 3`;
     do
         printtime docker-compose exec -T --user drupal drupal bash -c 'cd ~/public_html/web && drush -y si --account-name=blub --account-mail=bla@bla.com'
     done
 
 
-    echo "### $1: drush cr 5x"
-    for i in `seq 1 5`;
+    echo "### $1: drush cr 3x"
+    for i in `seq 1 3`;
     do
         printtime docker-compose exec -T --user drupal drupal bash -c 'cd ~/public_html/web && drush -y cr'
     done
@@ -32,11 +32,13 @@ function runTest {
 }
 
 echo "### CACHALOT"
+# Loading env variables so that we use Cachalot (Docker Machine)
 eval $(amazeeio-cachalot env)
 
 runTest cachalot
 
-
+# Removing Cachalot Docker Environment Variables again, now we use Docker for Mac
+# see https://docs.docker.com/docker-for-mac/docker-toolbox/
 unset ${!DOCKER_*}
 
 echo "### Docker for Mac - delegated"
